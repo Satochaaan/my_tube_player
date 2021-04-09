9c45193d5f7210be0f5cbdc834887e59f5eea8f4
Satochaaan <k97satosi327@gmail.com>
2021-04-10 08:31:02 +0900

    json用のmodelクラス作成

--- lib/model.dart
+++ lib/model.dart
@@ -1,22 +1,79 @@
-class SearchModel {
-  String videoId;
-  String thumbnailUrl;
-  String thumbnailWidth;
-  String thumbnailHeight;
-  String publishedAt;
-  String title;
-  String channelTitle;
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable(explicitToJson: true, createToJson: false)
+class Items {
+  List<Item> items = [];
+  Items(this.items);
+}
+
+@JsonSerializable(createToJson: false)
+class Item {
+  String kind;
+  String etag;
+  Id id;
+  Snippet snippet;
 
   // コンストラクタ
-  SearchModel(
-      {this.videoId,
-      this.thumbnailUrl,
-      this.thumbnailWidth,
-      this.thumbnailHeight,
-      this.publishedAt,
-      this.title,
-      this.channelTitle});
+  Item({
+    this.kind,
+    this.etag,
+    this.id,
+    this.snippet,
+  });
+}
 
-  factory SearchModel.fromJson(Map<String, dynamic> json) =>
-      SearchModel(videoId: json['videoId']);
+@JsonSerializable(createToJson: false)
+class Id {
+  String kind;
+  String videoId;
+  String channelId;
+  String playlistId;
+
+  // コンストラクタ
+  Id({
+    this.kind,
+    this.videoId,
+    this.channelId,
+    this.playlistId,
+  });
+}
+
+@JsonSerializable(explicitToJson: true, createToJson: false)
+class Snippet {
+  String publishedAt;
+  String channelId;
+  String title;
+  String description;
+  String channelTitle;
+
+  Snippet({
+    this.publishedAt,
+    this.channelId,
+    this.title,
+    this.description,
+    this.channelTitle,
+  });
+}
+
+@JsonSerializable(createToJson: false)
+class Thumbnails {
+  @JsonKey(name: 'default')
+  Thumbnail defaultThumbnail;
+  @JsonKey(name: 'medium')
+  Thumbnail mediumThumbnail;
+  @JsonKey(name: 'high')
+  Thumbnail highThumbnail;
+
+  Thumbnails({
+    this.defaultThumbnail,
+    this.mediumThumbnail,
+    this.highThumbnail,
+  });
+}
+
+@JsonSerializable(createToJson: false)
+class Thumbnail {
+  String url;
+  String width;
+  String height;
 }
